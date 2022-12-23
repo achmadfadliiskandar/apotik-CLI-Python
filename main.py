4b7a0eb9822318ae2f4fbd811f8eebce910ac47f
achmadfadliiskandar <af137357@gmail.com>
2022-12-23 16:19:40 +0700

    alhamdulillah kelar sebenarnya yang kepake itu cma data.csv sama yang apotek.py doang yang lain nya cma contoh aja wkwk biar lebih mudah memahami

--- main.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tkin
-first = tkin.Tk()
-# untuk mencetak label
-a = tkin.Label(first,text="Assalamualaikum wr wb")
-# untuk mencetak button
-b = tkin.Button(first,text="Klik aku dong")
-a.pack()
-b.pack()
-first.mainloop()
